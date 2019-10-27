0d3d9e6f84084fd60623809023ea9b39bd7ef02d
Shriya <shriya.shankar19@gmail.com>
2019-10-27 12:39:26 +0530

    iframe calendar code

---
+++ JScalendar/WebApplication5/Calendar.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calendar.aspx.cs" Inherits="WebApplication5.Calendar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="jquery.min.js"></script>
+    <script src="Calendar.js"></script>
+    <style type="text/css">
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <div>
+            <div id="Calendar"></div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ JScalendar/WebApplication5/Calendar.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function () {
+
+    GenerateHTMLString()
+
+});
+
+function GenerateHTMLString() {
+    var date = new Date();
+    var start = new Date();
+    var end = new Date();
+    start.setDate(date.getDate());
+    end.setDate(date.getDate() + 30);
+
+    var htmls = '';
+    htmls += '<table><tr><th> Name </th>';
+    for (var d = start ; d <= end; d.setDate(d.getDate() + 1)) {
+
+        var WGHeader = '<th>' + formatDateddMM(new Date(d)) + '</th>';
+        htmls += WGHeader;
+
+    }
+    htmls += '</tr>'
+    var onerow = '';
+    for (var i = 0; i < 5; i++) {
+
+        var name = "A" + i.toString();
+        onerow += '<tr><td>' + name + '</td>';
+
+
+        for (var ji = 0; ji <= 30; ji++) {
+
+            onerow += '<td> </td>';
+
+
+        }
+        onerow += '</tr>'
+    }
+    htmls += onerow;
+    $('#Calendar').append(htmls);
+
+}
+
+function formatDateddMM(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [day, month].join('/');
+}
