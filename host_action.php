e9d356e99dfd1092ec279084c3b91147b4730a67
Shriya <shriya.shankar19@gmail.com>
2019-10-27 20:18:17 +0530

    host tournament page

---
+++ host_action.php
@@ -0,0 +1,73 @@
+
+<!-- This code is executed after the form is submitted by user -->
+
+<!-- For sweet alert -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  </head>
+</html>
+
+<?php
+
+    // Credentials to access MySQL database
+    $servername = "localhost";
+    $username = "sportsforum";
+    $password = "sp0rtsf0rum";
+    $dbname = "sportsforum";
+    //$table = "data";
+    $table = "host_tournament";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else
+    {
+        echo "Connection successful <br>";
+    }
+
+    // Form values are mapped to database fields
+     
+        $sport = $_POST['sport'];
+        $host=$_POST['college'];
+        $start=$_POST['startdate'];
+        $end=$_POST['enddate'];
+        
+        // Query to insert values into database
+        $sql = "INSERT INTO host_tournament (Sport, Host, StartDate, EndDate)
+        VALUES ('$sport', '$host', '$start', '$end')";
+        echo "Record submitted. ";
+
+        if ($conn->query($sql) === TRUE)
+        {
+            echo "Redirecting... ";
+            echo("<script>swal({
+                  icon: 'success',
+                  title: 'Congratulations!',
+                  text: 'You are now a user of SportsForum',
+                  button: 'OK',
+                  closeOnClickOutside: false
+            }).then(function(){window.location='login.html'});</script>");
+        }
+        
+        // Error message for unsuccessful attempt
+        else
+        {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            echo("<script>swal({
+                  icon: 'error',
+                  title: 'Oops!',
+                  text: 'There's an error on our end :(. Contact us!',
+                  button: 'OK',
+                  closeOnClickOutside: false
+            }).then(function(){window.location='login.html'});</script>");
+        }
+        // Close connection
+        $conn->close();
+
+?>
