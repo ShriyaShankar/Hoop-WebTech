0cd89401386709b76618a38be96b65a1e1f66f75
Shriya <shriya.shankar19@gmail.com>
2019-10-27 16:15:56 +0530

    basic page layouts

---
+++ home.js
@@ -0,0 +1,11 @@
+function hostTournament(){
+    window.location="host.html";
+}
+
+function joinTournament(){
+    window.location="join.html";
+}
+
+function editTournament(){
+    window.location="edit.html";
+}
