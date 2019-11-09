7d8195967072d209b9b81d6f4e863e32737da12b
Shriya <shriya.shankar19@gmail.com>
2019-11-09 18:18:53 +0530

    testing php

--- join.php
+++ join.php
@@ -1,56 +1,26 @@
-
-<!-- This code is executed after the form is submitted by user -->
-
-<!-- For sweet alert -->
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  </head>
-</html>
-
 <?php
+$servername = "localhost";
+$username = "sportsforum";
+$password = "sp0rtsf0rum";
+$dbname = "sportsforum";
 
-    // Credentials to access MySQL database
-    $servername = "localhost";
-    $username = "sportsforum";
-    $password = "sp0rtsf0rum";
-    $dbname = "sportsforum";
-    //$table = "data";
-    $table = "host_tournament";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT Sport, Host FROM host_tournament";
+$result = $conn->query($sql);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Sport " . $row["sport"]. " - Host " . $row["college"]. " " . "<br>";
     }
-    else
-    {
-        echo "Connection successful <br>";
-    }
-
-    // Form values are mapped to database fields
-     
-        $sport = $_POST['sport'];
-        $host=$_POST['college'];
-        $start=$_POST['startdate'];
-        $end=$_POST['enddate'];
-        
-        // Query to insert values into database
-        $sql = "SELECT Sport, Host, StartDate, EndDate FROM host_tournament";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo $sport, $host, $start, $end;
-            }
-        } else {
-            echo "0 results";
-        }
-        // Close connection
-        $conn->close();
-
-?>
+} else {
+    echo "0 results";
+}
+$conn->close();
+?> 
